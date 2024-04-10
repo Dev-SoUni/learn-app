79de605c2b60e92fd7aee24d94a7edfce914c3d7
Byun SoYun <bsy1482@naver.com>
2024-04-10 17:05:07 +0900

    feat: 강좌 수정 UI

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,14 +1,67 @@
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+import { LayoutDashboard } from "lucide-react"
 
-export default function CourseIdPage({
+import { db } from "@/lib/db"
+import { IconBadge } from "@/components/ui/icon-badge"
+
+export default async function CourseIdPage({
   params
 }: {
   params: {
     courseId: string
   }
 }) {
+  const { userId } = auth()
+
+  if (!userId){
+    return redirect("/")
+  }
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  })
+
+  if (!course){
+    return redirect("/")
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId
+  ]
+
+  const totalFields = requiredFields.length
+  const completedFields = requiredFields.filter(Boolean).length
+
+  const completionText = `(${completedFields}/${totalFields})`
+
   return (
-    <div>
-      page courseid: {params.courseId}
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">
+            강좌 설정
+          </h1>
+          <span className="text-sm text-slate-700">
+            입력 항목 {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge size="sm" variant="success" icon={LayoutDashboard}/>
+            <h2 className="text-xl">
+              강좌를 수정하세요.
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

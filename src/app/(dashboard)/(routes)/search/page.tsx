44689ffb54d87774dfe79269f5a256ba56dcc778
Byun SoYun <bsy1482@naver.com>
2024-04-16 14:21:37 +0900

    feat: 강좌 검색

--- src/app/(dashboard)/(routes)/search/page.tsx
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,10 @@
 import React from "react"
 
 import { db } from "@/lib/db"
-import { Categories } from "./_components/categories";
+
+import { SearchInput } from "@/components/search-input"
+
+import { Categories } from "./_components/categories"
 
 export default async function SearchPage() {
   const categories = await db.category.findMany({
@@ -11,10 +14,15 @@ export default async function SearchPage() {
   })
 
   return (
-    <div className="p-6">
-      <Categories
-        items={categories}
-      />
-    </div>
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories
+          items={categories}
+        />
+      </div>
+    </>
   )
 }
--- src/components/navbar-routes.tsx
+++ src/components/navbar-routes.tsx
@@ -7,32 +7,41 @@ import { UserButton } from "@clerk/nextjs"
 import { LogOut } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
+import { SearchInput } from "@/components/search-input"
 
 export function NavbarRoutes() {
   const pathname = usePathname()
 
   const isTeacherPage = pathname?.startsWith("/teacher")
   const isPlayerPage = pathname?.includes("/chapter")
+  const isSearchPage = pathname === "/search"
 
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-        <Button size="sm" variant="ghost" asChild>
-          <Link href="/">
-            <LogOut className="h-4 w-4 mr-2"/>
-            <span>나가기</span>
-          </Link>
-        </Button>
-      ): (
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+      )}
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
           <Button size="sm" variant="ghost" asChild>
-            <Link href="/teacher/courses">
-            선생님 모드
+            <Link href="/">
+              <LogOut className="h-4 w-4 mr-2"/>
+              <span>나가기</span>
             </Link>
           </Button>
-      )}
-      <UserButton
-        afterSignOutUrl="/"
-      />
-    </div>
+        ): (
+            <Button size="sm" variant="ghost" asChild>
+              <Link href="/teacher/courses">
+              선생님 모드
+              </Link>
+            </Button>
+        )}
+        <UserButton
+          afterSignOutUrl="/"
+        />
+      </div>
+    </>
   )
 }

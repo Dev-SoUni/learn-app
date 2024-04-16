cf2eb83f2ead1d5b7411544bca7c5fe25e84e49e
Byun SoYun <bsy1482@naver.com>
2024-04-16 13:48:21 +0900

    feat: 강좌 카테고리 조회

--- src/app/(dashboard)/(routes)/search/page.tsx
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,20 @@
 import React from "react"
 
-export default function SearchPage() {
+import { db } from "@/lib/db"
+import { Categories } from "./_components/categories";
+
+export default async function SearchPage() {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  })
+
   return (
-    <div>fd</div>
+    <div className="p-6">
+      <Categories
+        items={categories}
+      />
+    </div>
   )
 }

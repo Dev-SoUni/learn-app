98274cd05616edf5d248a53de32eb54de2c4c5f0
Byun SoYun <bsy1482@naver.com>
2024-04-18 14:47:33 +0900

    feat: Stripe 결제 구현

--- src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,7 +1,12 @@
 "use client"
 
+import { useState } from "react"
+import { toast } from "sonner"
+import axios from "axios"
+
 import { Button } from "@/components/ui/button"
-import {formatPrice} from "@/lib/format";
+import { formatPrice } from "@/lib/format"
+
 
 interface CourseEnrollButtonProps {
   price: number
@@ -12,8 +17,24 @@ export function CourseEnrollButton({
   price,
   courseId,
 }: CourseEnrollButtonProps ) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true)
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`)
+      window.location.assign(response.data.url)
+    } catch {
+      toast.error("문제가 일어났습니다.")
+    } finally {
+      setIsLoading(false)
+    }
+  }
   return (
     <Button
+      onClick={onClick}
+      disabled={isLoading}
       size="sm"
       className="w-full md:w-auto"
     >
